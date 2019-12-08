cb50eefb82675f3baa76e490db1117f4ab5a2612
Sogik <55917460+Sogik@users.noreply.github.com>
2019-12-08 20:34:49 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+Nargen-Tools
+
+ Autor: Sogik
+
+★ DESCRIPCION:
+
+Es uns script que te instala las heramiebtas mas basicas de termux!
+
+★ DESCARGAS:
+
+• GITHUB: 
+https://github.com/Sogik/Nargen-Tools
+
+★ COMO INSTALAR:
+
+Es simple el script te instala todo las herramientas el mismo, solo hace falta ejecutarlo                                                                                                                                   
+
+★ INSTALACION RAPIDA:
+
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Sogik/Nargen-Tools
+ls
+cd Nargen-Tools
+ls
+chmod +x nargen-tools.sh
+ls
+bash nargen-tools.sh
+
+★ HERRAMIENTAS:
+
+● Nmap
+● Figlet
+● Metasploit
+● Routersploit
+● Git
+● Wget
+● Curl
+● PHP
+● Python
+● Python2
+● Python3
+
+✨Pronto Más!
+
+★ Testeado en : Windows / Linux / Termux
+
+
+                                       Sogik
---
+++ nargen-tools.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#Hecho poe Sogik
+"#############################################"
+echo Hecho Por Sogik 
+"##############################################"
+apt install figlet 
+figlet Nargen-Tools
+apt install sl 
+sl
+echo Nargen-Tools 
+"###################"
+
+figlet Herranientas Basicas
+"###################"
+apt install git
+apt install wget
+aot install curl
+"##################"
+
+figlet Nmap
+"###################"
+echo instalando Nmap
+apt install nmap
+"###################"
+
+figlet Python
+"###################"
+echo instalando Python
+apt install python python2 python3
+"###################"
+
+figlet PHP
+"###################"
+echo instalando PHP
+apt install php
+"###################"
+
+figlet Metasploit
+"###################"
+echo instalando Metasploit
+apt install metasploit
+"###################"
+
+figlet Routersploit
+"###################"
+echo instalando Routersploit
+git clone https://github.com/threat9/routersploit
+"###################"
+
